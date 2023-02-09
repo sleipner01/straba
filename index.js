9424197ef961d92a92075ad53211fa0c3debdcbf
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2023-02-09 18:06:34 +0100

    config: Added firebase

--- index.js
+++ index.js
@@ -2,4 +2,27 @@ import React from "react";
 import reactDom from "react-dom";
 import App from "./src/App.js"
 
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4pDwTBA5PLzV8L4LlvC5JvnWrDjgLXx8",
+  authDomain: "straba-64026.firebaseapp.com",
+  projectId: "straba-64026",
+  storageBucket: "straba-64026.appspot.com",
+  messagingSenderId: "417560124897",
+  appId: "1:417560124897:web:af4cdaa50391982bdb86a0",
+  measurementId: "G-FF8KE98449"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
 reactDom.render(<App />, document.getElementById("root"));
