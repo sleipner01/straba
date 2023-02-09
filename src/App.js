09c7d609e74608f2fa9f2a4a760b6219626c1710
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2023-02-09 19:36:17 +0100

    Added router, firebase auth, and inital pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React, {useState, useEffect} from 'react';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { BrowserRouter as Router} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <h1>Straβa!</h1>
-    </div>
+    <Router>
+          <h1>Straβa!</h1>
+        <div>
+          <section>                              
+              <Routes>                                                                        
+                 <Route path="/" element={<Home/>}/>
+                 <Route path="/signup" element={<Signup/>}/>
+                 <Route path="/login" element={<Login/>}/>
+              </Routes>                    
+          </section>
+        </div>
+      </Router>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// import * as firebaseui from 'firebaseui'
+// import 'firebaseui/dist/firebaseui.css'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4pDwTBA5PLzV8L4LlvC5JvnWrDjgLXx8",
+  authDomain: "straba-64026.firebaseapp.com",
+  projectId: "straba-64026",
+  storageBucket: "straba-64026.appspot.com",
+  messagingSenderId: "417560124897",
+  appId: "1:417560124897:web:af4cdaa50391982bdb86a0",
+  measurementId: "G-FF8KE98449"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
