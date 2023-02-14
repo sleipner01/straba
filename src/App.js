e601ab702857fe14acabc5c3394fde827cf0613a
Elin Haugum <elihaugu@stud.ntnu.no>
2023-02-14 18:44:36 +0100

    Add example for sidebar and topbar
    Needed to commit this or else it would tell me my files were deleted and would not let me work with them.

--- src/App.js
+++ src/App.js
@@ -4,31 +4,41 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 import './App.scss';
 
 import { auth } from './firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-import Sidebar from './components/navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
+import Topbar from './components/topbar/Topbar';
+import Sidebar from './components/sidebar/Sidebar';
 
 const App = ({ pagepath }) => {
   const [user, loading, error] = useAuthState(auth);
 
   return (
     <Fragment>
-      {!user && <Sidebar />}
-      <h1>Straβa!</h1>
-      <Router>
-        <div>
-          <section>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/signup' element={<Signup />} />
-              <Route path='/login' element={<Login />} />
-            </Routes>
-          </section>
-        </div>
-      </Router>
+      <Box>
+        <Topbar />
+      </Box>
+      <Box>
+        <Sidebar />
+      </Box>
+      <Box>
+        <h1>Straβa!</h1>
+        <Router>
+          <div>
+            <section>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/signup' element={<Signup />} />
+                <Route path='/login' element={<Login />} />
+              </Routes>
+            </section>
+          </div>
+        </Router>
+      </Box>
     </Fragment>
   );
 };
