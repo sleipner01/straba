82ef2600338de08bd616b2eb45f5eab3d0199095
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2023-02-15 08:31:08 +0100

    css: set layout to only show in home

--- src/App.js
+++ src/App.js
@@ -5,29 +5,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 
-import Topbar from './components/topbar/Topbar';
-import Sidebar from './components/sidebar/Sidebar';
-import { Box } from '@mui/system';
-
 function App() {
   return (
     <div className='mainContainer'>
-      <Topbar />
-      <Sidebar />
-      <Box className='content'>
-        <h1>Straβa!</h1>
-        <Router>
-          <div>
-            <section>
-              <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/signup' element={<Signup />} />
-                <Route path='/login' element={<Login />} />
-              </Routes>
-            </section>
-          </div>
-        </Router>
-      </Box>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
