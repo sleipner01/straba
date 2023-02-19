c63027a9a27becf5dd7c0879d6d67c724d80ee7d
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2023-02-19 22:28:03 +0100

    feat: moved logout to sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,40 @@
 import './sidebar.scss';
-import { RssFeed } from '@mui/icons-material';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { auth } from '../../firebase';
+import { signOut } from 'firebase/auth';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 export default function Sidebar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate('/login');
+        console.log('Signed out successfully');
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
   return (
     <div className='sidebar'>
       <div className='sidebarWrapper'>
         <ul className='sidebarList'>
           <li className='sidebarListItem'>
-            <RssFeed className='sidebarIcon' />
-            <span className='sidebarListItemText'>Workouts</span>
+            <NavLink to='/workouts'>
+              <FitnessCenterIcon className='sidebarIcon' />
+              <span className='sidebarListItemText'>Workouts</span>
+            </NavLink>
+          </li>
+          <li className='sidebarListItem'>
+            <NavLink onClick={handleLogout}>
+              <LogoutIcon className='sidebarIcon' />
+              <span className='sidebarListItemText'>Logout</span>
+            </NavLink>
           </li>
         </ul>
       </div>
