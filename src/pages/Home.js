632e25e79b80c912dc84854b920d8c55b40d2ba0
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2023-02-19 22:28:52 +0100

    fix: removed logout functionality from home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import { useEffect, Fragment, useRef } from 'react';
-import { signOut, onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebase';
-import { NavLink, useNavigate } from 'react-router-dom';
 
 import { Box } from '@mui/material';
 
@@ -16,38 +15,12 @@ const Home = () => {
     });
   }, []);
 
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    signOut(auth)
-      .then(() => {
-        // Sign-out successful.
-        navigate('/login');
-        console.log('Signed out successfully');
-      })
-      .catch((error) => {
-        // An error happened.
-      });
-  };
-
   return (
     <Fragment>
       <Box className='content'>
-        <h1>Straβa!</h1>
-        <div>
-          <section>
-            <nav>
-              <p>
-                Welcome <span ref={userDisplayName}></span>
-              </p>
-              <div>
-                <NavLink to='/login' onClick={handleLogout}>
-                  Logout
-                </NavLink>
-              </div>
-            </nav>
-          </section>
-        </div>
+        <h1>
+          Welcome <span ref={userDisplayName}></span>
+        </h1>
       </Box>
     </Fragment>
   );
