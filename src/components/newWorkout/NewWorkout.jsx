ba4c0d65d037bd66177713fad3a9a3b5bfdeee8f
Elin Haugum <elihaugu@stud.ntnu.no>
2023-02-22 02:19:32 +0100

    feat: Change newWorkout component to show correct data
    The component used to show program name and workouts, but should rather show workout name, activities and small info about each activity.
    
    Co-author: Aksel Hodne Hanisch @akselhh

--- src/components/newWorkout/NewWorkout.jsx
+++ src/components/newWorkout/NewWorkout.jsx
@@ -6,81 +6,8 @@ import CardActionArea from '@mui/material/CardActionArea';
 import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { TextField } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import './NewWorkout.scss';
-
-import FormControl from '@mui/material/FormControl';
-import NativeSelect from '@mui/material/NativeSelect';
-
-// test data
-const userObject = {
-  username: 'elihaugu',
-  programs: ['program1', 'program2'],
-  email: 'elihaugu@stud.ntnu.no',
-  fullname: 'Elin Haugum',
-};
-
-const program1 = {
-  name: 'program1',
-  workouts: [
-    {
-      name: 'workout1',
-      activities: [
-        {
-          name: 'activity1',
-          field1type: 'Reps',
-          field1value: 12,
-          field2type: 'Sets',
-          field2value: 3,
-        },
-        {
-          name: 'activity2',
-          field1type: 'Sec',
-          field1value: 30,
-          description: 'Run for 30 secs',
-        },
-      ],
-    },
-    {
-      name: 'workout2',
-      activities: [
-        {
-          name: 'activity1',
-          field1type: 'Secs',
-          field1value: 60,
-        },
-      ],
-    },
-  ],
-};
-
-const program2 = {
-  name: 'program2',
-  workouts: [
-    {
-      name: 'workout1',
-      activities: [
-        {
-          name: 'activity1',
-          field1type: 'Secs',
-          field1value: 30,
-          field2type: 'Sets',
-          field2value: 3,
-          description: 'Do the exercise 3 times.',
-        },
-        {
-          name: 'activity2',
-          field2type: 'Sec',
-          field2value: 30,
-          description: 'Run for 30 secs',
-        },
-      ],
-    },
-  ],
-};
-
-const programs = [program1, program2];
 
 // handle style for expansion of card, gives cool tapping effect
 const ExpandMore = styled((props) => {
@@ -105,56 +32,7 @@ const EditPage = styled((props) => {
   }),
 }));
 
-export default function NewWorkout() {
-  const regex = /^(\s*|\d+)$/;
-
-  // handle field1 state and illegal inputs
-  const [field1Value, setField1Value] = React.useState();
-  const [field1Type, setField1Type] = React.useState('reps');
-  const [errorField1, setErrorField1] = React.useState(false);
-
-  const handleSetField1 = (e) => {
-    // don't allow negative values or special characters
-    setErrorField1(e.target.value < 0 || !regex.test(e.target.value));
-    setField1Value(e.target.value);
-  };
-
-  // handle field2 state
-  const [field2Value, setField2Value] = React.useState();
-  const [field2Type, setField2Type] = React.useState('sets');
-  const [errorField2, setErrorField2] = React.useState(false);
-
-  const handleSetField2 = (e) => {
-    // don't allow negative values or special characters
-    setErrorField2(e.target.value < 0 || !regex.test(e.target.value));
-    setField2Value(e.target.value);
-  };
-
-  // handle set activityInfo at top of card
-  const [activityInfo, updateActivityInfo] = React.useState('');
-  const handleUpdateActivityInfo = () => {
-    // yes i know ugly asf if else but they don't look at the code and it was the easiest at the time
-    // don't hang me just for some ugly ifs pls
-    let activityInfo = '';
-    if (field1Value > 0 && !errorField1) {
-      activityInfo = activityInfo.concat(`${field1Value} ${field1Type}`);
-    }
-    if (field1Value > 0 && !errorField1 && field2Value > 0 && !errorField2) {
-      activityInfo = activityInfo.concat(' x ');
-    }
-    if (field2Value > 0 && !errorField2) {
-      activityInfo = activityInfo.concat(`${field2Value} ${field2Type}`);
-    }
-    updateActivityInfo(activityInfo);
-  };
-  // setup useEffect so activity info updates onchange to field1 and field2
-  React.useEffect(() => {
-    handleUpdateActivityInfo();
-  }, [field1Value, field2Value, field1Type, field2Type]);
-
-  // handle saving description
-  const [description, updateDescription] = React.useState('');
-
+export default function NewWorkout({ workout }) {
   // handle expanding card
   const [expanded, setExpanded] = React.useState(false);
   const handleExpandClick = (e) => {
@@ -162,8 +40,6 @@ export default function NewWorkout() {
       setExpanded(!expanded);
     }
   };
-
-  const string = 'wah her kommer firebase ting';
 
   return (
     <Card sx={{ maxWidth: 700, backgroundColor: '#FCE181' }}>
@@ -174,7 +50,7 @@ export default function NewWorkout() {
           handleExpandClick(e);
         }}
       >
-        <span id='workoutName'>{`${string}`}</span>
+        <span id='workoutName'>{`${workout.name}`}</span>
         <ExpandMore
           className='expandIcon'
           expand={expanded}
@@ -192,13 +68,13 @@ export default function NewWorkout() {
       </CardActionArea>
       <Collapse in={expanded} unmountOnExit>
         <CardContent className='expandedContent' sx={{ backgroundColor: '#FEF9C7' }}>
-          {programs.map((program) => {
+          {workout.activities.map((activity, index) => {
             return (
-              <div>
-                <p>{program.name}</p>
-                {program.workouts.map((workout) => {
-                  return <p>{workout.name}</p>;
-                })}
+              <div key={index}>
+                <span>{activity.name}</span>
+                <span className='activitySmallInfo'>{activity.field2value}</span>
+                <span className='activitySmallInfo'>{activity.field1value && activity.field2value ? 'x' : ''}</span>
+                <span className='activitySmallInfo'>{activity.field1value}</span>
               </div>
             );
           })}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,76 @@ import { auth } from '../firebase';
 
 import NewWorkout from '../components/newWorkout/NewWorkout';
 
+// test data
+const program1 = {
+  id: 2,
+  name: 'program1',
+  workouts: [
+    {
+      name: 'workout1',
+      activities: [
+        {
+          name: 'activity1',
+          field1type: 'Reps',
+          field1value: 12,
+          field2type: 'Sets',
+          field2value: 3,
+        },
+        {
+          name: 'activity2',
+          field1type: 'Sec',
+          field1value: 30,
+          description: 'Run for 30 secs',
+        },
+      ],
+    },
+    {
+      name: 'workout2',
+      activities: [
+        {
+          name: 'activity1',
+          field1type: 'Secs',
+          field1value: 60,
+        },
+      ],
+    },
+  ],
+};
+
+const userObject = {
+  id: 1,
+  username: 'elihaugu',
+  programs: [2, 3],
+  email: 'elihaugu@stud.ntnu.no',
+  fullname: 'Elin Haugum',
+};
+
+const program2 = {
+  id: 3,
+  name: 'program2',
+  workouts: [
+    {
+      name: 'workout1',
+      activities: [
+        {
+          name: 'activity1',
+          field1type: 'Secs',
+          field1value: 30,
+          field2type: 'Sets',
+          field2value: 3,
+          description: 'Do the exercise 3 times.',
+        },
+        {
+          name: 'activity2',
+          field2type: 'Sec',
+          field2value: 30,
+          description: 'Run for 30 secs',
+        },
+      ],
+    },
+  ],
+};
+
 const Home = () => {
   const userDisplayName = useRef();
 
@@ -20,7 +90,7 @@ const Home = () => {
       <h1>
         Welcome <span ref={userDisplayName}></span>
       </h1>
-      <NewWorkout />
+      <NewWorkout workout={program1.workouts[0]} />
     </div>
   );
 };
