db7fcce34d50e410f050ae3e9c9326b76227e6df
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2023-03-06 19:28:46 +0100

    progress: inputs

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -2,6 +2,15 @@ const Settings = () => {
   return (
     <div>
       <h1>Settings</h1>
+      <form>
+        <label htmlFor='name'>Name</label>
+        <input type='text' name='name' id='name' />
+        <br />
+        <label htmlFor='phoneNumber'>Phone Number</label>
+        <input type='tel' name='phoneNumber' id='phoneNumber' />
+        <br />
+        <button type='submit'>Update</button>
+      </form>
     </div>
   );
 };
