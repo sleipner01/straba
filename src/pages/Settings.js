45309bb945f628ce57faa38451834cfe9daa449b
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2023-03-07 09:41:19 +0100

    progress: validation and name-update

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,15 +1,70 @@
+import { useState, useEffect } from 'react';
+import { Error } from '../components/misc/usefulComponents';
+import { auth } from '../../firebase';
+import { updateProfile } from 'firebase/auth';
+
 const Settings = () => {
+  const [name, setName] = useState('');
+  const [nameValid, setNameValidState] = useState(false);
+  const [submitDisabled, setSubmitDisabledState] = useState(true);
+  const [errorText, setError] = useState();
+
+  useEffect(() => {
+    checkSubmitValid();
+  }, [nameValid]);
+
+  function checkSubmitValid() {
+    // check if all fields are valid
+    let submitValid = nameValid;
+    setSubmitDisabledState(!submitValid);
+  }
+
+  function handleChangeName(name) {
+    // separate handler for each field
+    let nameValid = name ? true : false; // basic text validation
+    setName(name);
+    setNameValidState(nameValid);
+  }
+
+  const updateAuthName = async () => {
+    updateProfile(auth.currentUser, {
+      displayName: name,
+    })
+      .then(() => {
+        console.log('Updated auth name');
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.message);
+      });
+  };
+
   return (
     <div>
       <h1>Settings</h1>
       <form>
-        <label htmlFor='name'>Name</label>
-        <input type='text' name='name' id='name' />
+        <label className='label' htmlFor='name'>
+          Name
+        </label>
+        <input
+          type='text'
+          name='name'
+          id='name'
+          placeholder='Name'
+          className='input'
+          value={name}
+          onChange={(e) => handleChangeName(e.target.value)}
+        />
         <br />
-        <label htmlFor='phoneNumber'>Phone Number</label>
-        <input type='tel' name='phoneNumber' id='phoneNumber' />
+        <label className='label' htmlFor='phoneNumber'>
+          Phone Number
+        </label>
+        <input type='tel' name='phoneNumber' id='phoneNumber' placeholder='Phone Number' className='input' required />
         <br />
-        <button type='submit'>Update</button>
+        <button disabled={submitDisabled} type='submit' onClick={updateAuthName} required>
+          Update
+        </button>
+        {errorText && <Error errorMessage={errorText} />}
       </form>
     </div>
   );
